dba80e62d18c39e34f7b5c20643fa25b2ec24a5d
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-12 20:07:11 +0100

    add migrate concour

---
+++ database/migrations/2021_12_12_174641_create_concours_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConcoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('concours', function (Blueprint $table) {
+            $table->id();
+            $table->boolean("designation");
+            $table->string("etap");
+            $table->string("poursentage");
+            $table->double('latitude');
+            $table->double('longitude');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('concours');
+    }
+}
