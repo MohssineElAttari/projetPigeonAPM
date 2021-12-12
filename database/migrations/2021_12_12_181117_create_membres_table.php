372db4182d44774ad97ffe00193cb493a5bb17e9
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-12 20:10:04 +0100

    add migrate membre

---
+++ database/migrations/2021_12_12_181117_create_membres_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('membres', function (Blueprint $table) {
+            $table->id();
+            $table->string("nom_francais");
+            $table->string("nom_arabe");
+            $table->string("prenom_francais");
+            $table->string("prenom_arabe");
+            $table->double('latitude');
+            $table->double('longitude');
+            $table->string("email");
+            $table->integer("tel");
+            $table->string("type");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('membres');
+    }
+}
