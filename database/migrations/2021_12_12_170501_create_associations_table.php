15ae00eeb7e8837a84a0d92c83f2391680c3f3a9
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-12 20:04:17 +0100

    add migrate association

---
+++ database/migrations/2021_12_12_170501_create_associations_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssociationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('associations', function (Blueprint $table) {
+            $table->id();
+            $table->string("nom");
+            $table->string("prenom");
+            $table->string("abreviation");
+            $table->integer("tel");
+            $table->integer("fix");
+            $table->string("email");
+            $table->text("adress");
+            $table->string("pays");
+            $table->string("ville");
+            $table->string("page_facebook");
+            $table->boolean("active");
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('associations');
+    }
+}
