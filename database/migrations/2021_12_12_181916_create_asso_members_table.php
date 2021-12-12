ddaf7b51b70ab57a58bc43f04c95b894c98e56ec
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-12 20:15:17 +0100

    add migrate membre

---
+++ database/migrations/2021_12_12_181916_create_asso_members_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssoMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asso_members', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('association_id')->constrained();
+            $table->foreignId('membre')->constrained();
+            $table->date("date_inscription");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asso_members');
+    }
+}
