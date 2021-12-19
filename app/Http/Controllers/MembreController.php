e8595529820806e14a4f565f4809e9aaba8c9c27
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-19 20:10:11 +0100

    new update in membre Controller

--- app/Http/Controllers/MembreController.php
+++ app/Http/Controllers/MembreController.php
@@ -3,10 +3,97 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class MembreController extends Controller
 {
-   public function index(){
-       return view('dashboard/pages/membres');
-   }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // dd(Auth::id());
+        $id = DB::table('association_groups')
+            ->join('users', 'association_groups.user_id', '=', 'users.id')
+            ->select('association_groups.id')
+            ->where('association_groups.user_id', Auth::id())
+            ->first()->id;
+        $membres = DB::table('membres')
+            ->join('asso_members', 'asso_members.membre_id', '=', 'membres.id')
+            ->select('membres.*')
+            ->where('asso_members.association_groups_id', $id)
+            ->get();
+            // dd($membres);
+
+        return view('dashboard/pages/membres',['membres' => $membres]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
