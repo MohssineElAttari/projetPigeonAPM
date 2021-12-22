ddcb6f2dee8b6008573761b692e59fc4e81d9919
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-22 15:14:31 +0100

    update routes

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Auth::routes();
 
-Route::middleware(['CheckActive','auth'])->group(function () {
-    Route::get('/home', [HomeController::class, 'index'])->name('home'); 
-    Route::get('/dashboard/membre',[MembreController::class,'index'])->name('membre');
-    Route::post('/dashboard/membre',[MembreController::class,'store'])->name('membre.store');
-    Route::post('/dashboard/membre/delete/{id}',[MembreController::class,'destroy'])->name('membre.delete');
-    Route::post('/dashboard/membre/update/{id}',[MembreController::class,'update'])->name('membre.update');
-    Route::get('/dashboard/concour',[ConcourController::class,'index'])->name('concour');
-    Route::get('/logout', [LogoutController::class,'logout'])->name('logout.perform');
+Route::middleware(['CheckActive', 'auth'])->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/dashboard/membre', [MembreController::class, 'index'])->name('membre');
+    Route::post('/dashboard/membre', [MembreController::class, 'store'])->name('membre.store');
+    Route::post('/dashboard/membre/delete/{id}', [MembreController::class, 'destroy'])->name('membre.delete');
+    Route::post('/dashboard/membre/update/{id}', [MembreController::class, 'update'])->name('membre.update');
+    Route::get('/dashboard/concour', [ConcourController::class, 'index'])->name('concour');
+    Route::get('/logout', [LogoutController::class, 'logout'])->name('logout.perform');
+    Route::get('file-import-export', [MembreController::class, 'fileImportExport']);
+    Route::post('file-import', [MembreController::class, 'fileImport'])->name('file-import');
+    Route::get('file-export', [MembreController::class, 'fileExport'])->name('file-export');
 });
 
 Route::get('/', function () {
