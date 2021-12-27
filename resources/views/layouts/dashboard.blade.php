1b17e88d3cc78199a2737882fb2c27ea55768553
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-27 22:45:29 +0100

    update in dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -13,72 +13,37 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-
-    <link rel="apple-touch-icon" href="{{ asset('images/dashboard/ico/apple-icon-120.png') }}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/dashboard/ico/favicon.ico') }}">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600"
-        rel="stylesheet">
+    <link rel="apple-touch-icon" href="{{ asset('app-assets/images/ico/apple-icon-120.png') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('app-assets/images/ico/favicon.ico') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('vendors/dashboard/css/vendors.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('vendors/dashboard/css/charts/apexcharts.css') }}">
-    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('vendors/dashboard/css/extensions/toastr.min.css') }}"> --}}
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/vendors.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/charts/apexcharts.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/extensions/toastr.min.css') }}">
     <!-- END: Vendor CSS-->
 
     <!-- BEGIN: Theme CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/dashboard/bootstrap.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/dashboard/bootstrap-extended.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/dashboard/colors.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/dashboard/components.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/dashboard/themes/dark-layout.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/dashboard/themes/bordered-layout.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/dashboard/themes/semi-dark-layout.css') }}">
-
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap-extended.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/colors.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/components.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/dark-layout.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/bordered-layout.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/semi-dark-layout.css') }}">
 
     <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css"
-        href="{{ asset('css/dashboard/core/menu/menu-types/horizontal-menu.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/dashboard/pages/dashboard-ecommerce.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/dashboard/plugins/charts/chart-apex.css') }}">
-    <link rel="stylesheet" type="text/css"
-        {{-- href="{{ asset('css/dashboard/plugins/extensions/ext-component-toastr.css') }}"> --}}
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/core/menu/menu-types/horizontal-menu.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/pages/dashboard-ecommerce.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/plugins/charts/chart-apex.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/plugins/extensions/ext-component-toastr.css') }}">
     <!-- END: Page CSS-->
 
     <!-- BEGIN: Custom CSS-->
-    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('css/dashboard/style.css') }}"> --}}
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
     <!-- END: Custom CSS-->
 
-    <!-- Scripts -->
-    <!-- BEGIN: Vendor JS-->
-    <script defer src="{{ asset('vendors/dashboard/js/vendors.min.js') }}"></script>
-    <!-- BEGIN Vendor JS-->
-
-    <!-- BEGIN: Page Vendor JS-->
-    <script defer src="{{ asset('vendors/dashboard/js/ui/jquery.sticky.js') }}"></script>
-    {{-- <script defer src="{{ asset('vendors/dashboard/js/charts/apexcharts.min.js') }}"></script> --}}
-    {{-- <script defer src="{{ asset('vendors/dashboard/js/extensions/toastr.min.js') }}"></script> --}}
-    <!-- END: Page Vendor JS-->
-
-    <!-- BEGIN: Theme JS-->
-    <script defer src="{{ asset('js/dashboard/core/app-menu.js') }}"></script>
-    <script defer src="{{ asset('js/dashboard/core/app.js') }}"></script>
-    <!-- END: Theme JS-->
-
-    <!-- BEGIN: Page JS-->
-    <script defer src="{{ asset('js/dashboard/scripts/pages/dashboard-ecommerce.js') }}"></script>
-    <!-- END: Page JS-->
-
-    {{-- <script defer>
-        $(window).on('load', function() {
-            if (feather) {
-                feather.replace({
-                    width: 14,
-                    height: 14
-                });
-            }
-        })
-    </script> --}}
-    {{-- end script --}}
+    
     @stack('head')
 
 
@@ -97,6 +62,32 @@
 
     </div>
 
+      <!-- BEGIN: Vendor JS-->
+      <script src="{{ asset('app-assets/vendors/js/vendors.min.js') }}"></script>
+      <!-- BEGIN Vendor JS-->
+  
+      <!-- BEGIN: Page Vendor JS-->
+      <script src="{{ asset('app-assets/vendors/js/ui/jquery.sticky.js') }}"></script>
+      <script src="{{ asset('app-assets/vendors/js/charts/apexcharts.min.js') }}"></script>
+      <script src="{{ asset('app-assets/vendors/js/extensions/toastr.min.js') }}"></script>
+      <!-- END: Page Vendor JS-->
+  
+      <!-- BEGIN: Theme JS-->
+      <script src="{{ asset('app-assets/js/core/app-menu.js') }}"></script>
+      <script src="{{ asset('app-assets/js/core/app.js') }}"></script>
+      <!-- END: Theme JS-->
+  
+    <script defer>
+        $(window).on('load', function() {
+            if (feather) {
+                feather.replace({
+                    width: 14,
+                    height: 14
+                });
+            }
+        })
+    </script>
+    {{-- end script --}}
 </body>
 
 </html>
