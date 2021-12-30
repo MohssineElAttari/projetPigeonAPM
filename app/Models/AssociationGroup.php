d6d3e03308c6a6405fb0660b71c3accbf691edd4
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-30 18:14:35 +0100

    new update in AssociationGroup

--- app/Models/AssociationGroup.php
+++ app/Models/AssociationGroup.php
@@ -4,25 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\User as Authenticatable;  
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
 class AssociationGroup extends Authenticatable
 {
     use HasFactory;
     protected $fillable = [
         'logo',
-            'nom_asso',
-            'type',
-            'prenom_res',
-            'nom_res',
-            'abreviation',
-            'tel',
-            'fix',
-            'adresse',
-            'pays',
-            'ville',
-            'page',
-            'email',
-            'password',
-            'user_id'
+        'nom_asso',
+        'type',
+        'prenom_res',
+        'nom_res',
+        'abreviation',
+        'tel',
+        'fix',
+        'adresse',
+        'pays',
+        'ville',
+        'page',
+        'email',
+        'password',
+        'user_id'
     ];
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'foreign_key', 'owner_key');
+    }
+    public function concour()
+    {
+        return $this->hasMany(Concour::class, 'foreign_key', 'local_key');
+    }
+    public function members()
+    {
+        return $this->hasMany(Membre::class);
+    }
 }
