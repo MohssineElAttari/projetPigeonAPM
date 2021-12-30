53606a00d729944964e315eeadfba73b2fc3a732
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-30 18:15:26 +0100

    new update in model concour

--- app/Models/Concour.php
+++ app/Models/Concour.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Concour extends Model
 {
     use HasFactory;
+
+    /**
+ * Get the post that owns the comment.
+ */
+public function associationGroup()
+{
+    return $this->belongsTo(AssociationGroup::class, 'foreign_key', 'owner_key');
+}
 }
