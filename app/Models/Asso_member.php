abb76b1a4ce7979e889d9f95ae0623e67f30894b
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-30 19:10:47 +0100

    update in Models Asso_member

--- app/Models/Asso_member.php
+++ app/Models/Asso_member.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class Asso_member extends Model
+class Asso_member extends Pivot
 {
     use HasFactory;
+    public $incrementing = true;
 
     protected $fillable = [
         'association_groups_id',
