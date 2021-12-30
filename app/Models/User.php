ec1cd0339c9792ba2067b6611004644e52287e26
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-30 18:18:50 +0100

    update in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function AssociatioGroupe()
+    {
+        return $this->hasOne(AssociationGroup::class, 'foreign_key', 'local_key');
+    }
 }
