949fe3c000ba868bd24719354c2707395c501542
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-30 18:13:43 +0100

    new updat in membre export

--- app/Exports/MembreExport.php
+++ app/Exports/MembreExport.php
@@ -4,9 +4,15 @@ namespace App\Exports;
 
 use App\Models\Membre;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class MembreExport implements FromCollection
+class MembreExport implements FromCollection,WithHeadings
 {
+    public function headings(): array
+    {
+        return ["id", "nom_francais", "nom_arabe","prenom_francais","prenom_arabe","latitude","longitude","email","tel","created_at","updated_at"];
+    }
+
     /**
     * @return \Illuminate\Support\Collection
     */
