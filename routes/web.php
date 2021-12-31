548754c6fa3f3f1edd012ea00a85da5bf7faaea5
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-31 01:13:25 +0100

    update routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\ConcourController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ImportController;
 use App\Http\Controllers\MembreController;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
@@ -29,11 +28,12 @@ Route::middleware(['CheckActive', 'auth'])->group(function () {
     Route::post('/dashboard/membre/delete/{id}', [MembreController::class, 'destroy'])->name('membre.delete');
     Route::post('/dashboard/membre/update/{id}', [MembreController::class, 'update'])->name('membre.update');
 
-    Route::get('/import-membre', [ImportController::class, 'index'])->name('membre-mmport');
-    Route::get('/import-membre/fetch_data', [ImportController::class, 'fetch_data']);
-    Route::post('/import-membre/add_data', [ImportController::class, 'add_data'])->name('membre.add_data');
-    Route::post('/import-membre/update_data', [ImportController::class, 'update_data'])->name('membre.update_data');
-    Route::post('/import-membre/delete_data', [ImportController::class, 'delete_data'])->name('membre.delete_data');
+    Route::get('/import-membre', [MembreController::class, 'showdata'])->name('membre-mmport');
+    Route::get('/import-membre/fetch_data', [MembreController::class, 'fetch_data']);
+    Route::post('/import-membre/add_data', [MembreController::class, 'add_data'])->name('membre.add_data');
+    Route::post('/import-membre/update_data', [MembreController::class, 'update_data'])->name('membre.update_data');
+    Route::post('/import-membre/delete_data', [MembreController::class, 'delete_data'])->name('membre.delete_data');
+
     Route::get('file-import-export', [MembreController::class, 'fileImportExport']);
     Route::post('file-import', [MembreController::class, 'fileImport'])->name('file-import');
     Route::get('file-export', [MembreController::class, 'fileExport'])->name('file-export');
