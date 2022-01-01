ab0b98738d3a2f8ca6e99d096073b4f3620e4385
Mohssine Elattari <elattari.mohssine@gmail.com>
2022-01-01 03:57:46 +0100

    new update in import js

--- public/js/dashboard/import.js
+++ public/js/dashboard/import.js
@@ -32,26 +32,11 @@ $(document).ready(function () {
     }
     function show_data(result) {
         console.log("===>", result);
-        if (result.length != 0) {
 
-            var html = '';
-            html += '<tr>';
-            html += '<td contenteditable id="prenom francais"></td>';
-            html += '<td contenteditable id="prenom arabe"></td>';
-            html += '<td contenteditable id="nom francais"></td>';
-            html += '<td contenteditable id="nom arabe"></td>';
-            html += '<td contenteditable id="longitude"></td>';
-            html += '<td contenteditable id="latitude"></td>';
-            html += '<td contenteditable id="email"></td>';
-            html += '<td contenteditable id="tel"></td>';
-            html += '<td contenteditable id="asso"></td>';
-
-            html +=
-                '<td><button type="button" class="btn btn-success btn-xs" id="add">Add</button></td></tr>';
+        var html = '';
+        if (result.length > 0) {
             for (var count = 0; count < result.length; count++) {
-                // if (result[count]) {
 
-                // }
                 console.log(result[count]);
                 html += `<tr ${result[count]?.exist == 1 ? 'style="background-color:#ea5455a6;"' : ''}>`;
                 html +=
@@ -79,74 +64,42 @@ $(document).ready(function () {
                     '<td contenteditable class="column_name" data-column_name="tel" data-id="' +
                     result[count].id + '" data-index="' + count + '">' + result[count].tel + '</td>';
                 html +=
-                    '<td contenteditable class="column_name" data-column_name="asso" data-id="' +
+                    '<td class="column_name" data-column_name="asso" data-id="' +
                     result[count].id + '" data-index="' + count + '">' + result[count]['nom association'] + '</td>';
                 html +=
                     '<td><button type="button" class="btn btn-danger btn-xs delete" id="' +
                     result[count].id + '">Delete</button></td></tr>';
             }
-            $('tbody').html(html);
         } else {
             $('#messageImport').html("<div class='alert-message alert alert-danger p-1'>Le fichier que vous souhaitez importer est vide</div>");
             $(".alert-message").alert();
             window.setTimeout(function () { $(".alert-message").alert('close'); }, 5000);
         }
+        $('tbody').html(html);
     }
 
-    // function fetch_data() {
-    //     $.ajax({
-    //         url: "import-membre/fetch_data",
-    //         dataType: "json",
-    //         success: function (data) {
-    //             console.log(data);
-    //             var html = '';
-    //             html += '<tr>';
-    //             html += '<td contenteditable id="prenom_francais"></td>';
-    //             html += '<td contenteditable id="nom_francais"></td>';
-    //             html += '<td contenteditable id="longitude"></td>';
-    //             html += '<td contenteditable id="latitude"></td>';
-    //             html += '<td contenteditable id="email"></td>';
-    //             html += '<td contenteditable id="tel"></td>';
-    //             html +=
-    //                 '<td><button type="button" class="btn btn-success btn-xs" id="add">Add</button></td></tr>';
-    //             for (var count = 0; count < data.length; count++) {
-    //                 console.log(data[count]);
-    //                 html += '<tr>';
-    //                 html +=
-    //                     '<td contenteditable class="column_name" data-column_name="prenom_francais" data-id="' +
-    //                     data[count].id + '">' + data[count].prenom_francais + '</td>';
-    //                 html +=
-    //                     '<td contenteditable class="column_name" data-column_name="nom_francais" data-id="' +
-    //                     data[count].id + '">' + data[count].nom_francais + '</td>';
-    //                 html +=
-    //                     '<td contenteditable class="column_name" data-column_name="longitude" data-id="' +
-    //                     data[count].id + '">' + data[count].longitude + '</td>';
-    //                 html +=
-    //                     '<td contenteditable class="column_name" data-column_name="latitude" data-id="' +
-    //                     data[count].id + '">' + data[count].latitude + '</td>';
-    //                 html +=
-    //                     '<td contenteditable class="column_name" data-column_name="email" data-id="' +
-    //                     data[count].id + '">' + data[count].email + '</td>';
-    //                 html +=
-    //                     '<td contenteditable class="column_name" data-column_name="tel" data-id="' +
-    //                     data[count].id + '">' + data[count].tel + '</td>';
-    //                 html +=
-    //                     '<td><button type="button" class="btn btn-danger btn-xs delete" id="' +
-    //                     data[count].id + '">Delete</button></td></tr>';
-    //             }
-    //             $('tbody').html(html);
-    //         }
-    //     });
-    // }
 
     var _token = $('input[name="_token"]').val();
 
+    function notExisting() {
+        let numExist = result.reduce(function (n, membre) {
+            return n + (membre.exist != 0);
+        }, 0);
+        console.log(numExist);
+        if (numExist === 0) {
+            console.log(numExist);
+            $('#analiser').hide();
+            $('#enregistrer').removeAttr('hidden')
+            $('#message').html("<div class='alert-message alert alert-secondary p-1'>Les données sont prêtes à être enregistrées, appuyez sur enregistrer</div>");
+            $(".alert-message").alert();
+            window.setTimeout(function () { $(".alert-message").alert('close'); }, 5000);
+        }
+    }
 
     $(document).on('click', '#analiser', function () {
         // console.log('done ');
         console.log(result);
         if (result.length != 0) {
-            console.log("Full of");
             $.ajax({
                 url: analiseData,
                 dataType: "json",
@@ -163,6 +116,70 @@ $(document).ready(function () {
                     // data = JSON.parse(data);
                     console.log(data);
                     result = data;
+                    notExisting();
+                    show_data(result);
+                }
+            });
+        } else {
+            $('#message').html("<div class='alert-message alert alert-danger p-1'>Il n\'y a pas de données, faites une importation des données</div>");
+            $(".alert-message").alert();
+            window.setTimeout(function () { $(".alert-message").alert('close'); }, 3000);
+            // show_data();
+            console.log("Empty");
+        }
+    });
+
+    $(document).on('blur', '.column_name', function () {
+        var column_name = $(this).data("column_name");
+        var column_value = $(this).text();
+        var index = $(this).data("index")
+        var id = $(this).data("id");
+
+        if (column_value != '') {
+            // result[id];
+            console.log(result[index]);
+            result[index][column_name] = column_value;
+            // obj[column_name] = column_value;
+            $('#message').html("<div class='alert-message alert alert-success p-1'>Modifié avec succès</div>");
+            $(".alert-message").alert();
+            window.setTimeout(function () { $(".alert-message").alert('close'); }, 3000);
+        } else {
+            $('#message').html("<div class='alert alert-danger'>Enter some value</div>");
+        }
+    });
+
+    $(document).on('click', '.delete', function () {
+        var id = $(this).attr("id");
+        if (confirm("Are you sure you want to delete this records?")) {
+            result = result.filter(item => item.id != id);
+            show_data(result);
+            $('#message').html("<div class='alert-message alert alert-success p-1'>Supprimé avec succès</div>");
+            $(".alert-message").alert();
+            window.setTimeout(function () { $(".alert-message").alert('close'); }, 3000);
+
+        }
+    });
+
+    $(document).on('click', '#enregistrer', function () {
+
+        if (result.length != 0) {
+            $.ajax({
+                url: analiseData,
+                dataType: "json",
+                method: "POST",
+                data: {
+                    result: result,
+                    _token: _token,
+                },
+                success: function (data) {
+                    // $('#message').html(data);
+                    // $(".alert-message").alert();
+                    // window.setTimeout(function () { $(".alert-message").alert('close'); }, 3000);
+                    // fetch_data();
+                    // data = JSON.parse(data);
+                    console.log(data);
+                    result = data;
+                    notExisting();
                     show_data(result);
                 }
             });
@@ -208,36 +225,6 @@ $(document).ready(function () {
     //     }
     // });
 
-    $(document).on('blur', '.column_name', function () {
-        var column_name = $(this).data("column_name");
-        var column_value = $(this).text();
-        var index = $(this).data("index")
-        var id = $(this).data("id");
-
-        if (column_value != '') {
-            // result[id];
-            console.log(result[index]);
-            result[index][column_name] = column_value;
-            // obj[column_name] = column_value;
-            $('#message').html("<div class='alert-message alert alert-success p-1'>Modifié avec succès</div>");
-            $(".alert-message").alert();
-            window.setTimeout(function () { $(".alert-message").alert('close'); }, 3000);
-        } else {
-            $('#message').html("<div class='alert alert-danger'>Enter some value</div>");
-        }
-    });
-
-    $(document).on('click', '.delete', function () {
-        var id = $(this).attr("id");
-        if (confirm("Are you sure you want to delete this records?")) {
-            result = result.filter(item => item.id != id);
-            $('#message').html("<div class='alert-message alert alert-success p-1'>Supprimé avec succès</div>");
-            $(".alert-message").alert();
-            window.setTimeout(function () { $(".alert-message").alert('close'); }, 3000);
-            show_data(result);
-
-        }
-    });
 
 
 });
