8bf1a28a6b823611360f2f3f18997b64bce9c53c
Mohssine Elattari <elattari.mohssine@gmail.com>
2022-01-01 17:09:57 +0100

    new update in concour model

--- app/Models/Concour.php
+++ app/Models/Concour.php
@@ -7,8 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Concour extends Model
 {
+    // protected $table = "concours";
     use HasFactory;
-
+    protected $fillable = [
+        'association_group_id'
+    ];
     /**
  * Get the post that owns the comment.
  */
